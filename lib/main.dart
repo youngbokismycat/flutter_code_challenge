ade8143f4b880894d5f9c10cf466838b2dfa41d6
Young Jeen <a01023620369@gmail.com>
2024-06-13 17:28:33 +0900

    preview 패키지 설치

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tread_clone_assignment/consts/custom_text_style.dart';
@@ -8,7 +9,12 @@ void main() {
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle.dark,
   );
-  runApp(const MyApp());
+  runApp(DevicePreview(
+      enabled: false,
+      tools: const [
+        ...DevicePreview.defaultTools,
+      ],
+      builder: (context) => const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
