f32ba2c12e66fbfb102888be212b16581c8307fc
youngbok <a01023620369@gmail.com>
2024-07-02 16:05:41 +0900

    add ffmpeg

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,4 +9,6 @@ admin.initializeApp();
 
 export const onThreadCreadted = functions.firestore.document('thread/{threadId}').onCreate(async (snapshot, context) => {
  await snapshot.ref.update({hello:'from functions'});
+ const spawn = require('child-process-promise').spawn;
+ await spawn("ffmpeg")
 });
