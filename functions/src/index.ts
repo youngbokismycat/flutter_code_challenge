ad25779ffa3d68739d515caca84dcaa0eb326b4f
youngbok <a01023620369@gmail.com>
2024-07-02 15:40:59 +0900

    implement firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger"
+
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
